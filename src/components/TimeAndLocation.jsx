3d2cd8c95bdafe8406f6edae6c18d93d7dfe4667
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-04 13:08:16 +0700

    Time and Location

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+            <div className="text-white text-xl font-extralight">
+                Thursday, 04 Augustus 2022 | Local Time: 12:17 PM
+            </div>
+        </div>
+        <div className="flex items-center justify-center my-3">
+            <div className="text-white text-3xl font-medium">
+                Berlin, DE
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
