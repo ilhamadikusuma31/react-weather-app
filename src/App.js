4fef69c39fb5dd7184a216d826642494eafa99ca
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-06 13:58:46 +0700

    api mati, project hampir done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useEffect, useState } from "react";
 
 
 function App() {
-  const [query, setQuery] = useState({q:"berlin"});
+  const [query, setQuery] = useState({q:"jakarta"});
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
 
@@ -23,6 +23,15 @@ function App() {
     fw();
 
   }, [query,units,weather]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
